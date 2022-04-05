cf88a9281e2f98d0bc4b927d8375ed6a00e50d59
otsokeuda <otso.rantala@edu.keuda.fi>
2022-04-05 12:33:14 +0300

    items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-
+import React from 'react';
 import './App.css';
 import Instructions from './Instructions';
 import Counter from './Counter';
 import Link from './Link';
 
 function App() {
-  const tehtavaLista = ["aaaaaa", "bbbbbbb", "cccccccc", "dddddddd"];
+  const tehtavaLista = [
+    {
+      id: 1,
+      title: 'Osta banaania',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Soita kaverille',
+      completed: false,
+    },
+  ];
+  
+  const [items, setItems] = React.useState(tehtavaLista);
 
   return (
     <div className="App">
       <header className="App-header">
         <Counter list={tehtavaLista.length}/>
 
-        {tehtavaLista.map(tehtava => <Instructions kuvaus={tehtava} />)}
+        {items.map(tehtava => <Instructions kuvaus={tehtava.title} />)}
 
         <Link text="tekstiä" target="https://reactjs.org" />
       </header>
